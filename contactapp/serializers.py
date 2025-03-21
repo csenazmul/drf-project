2e289bbf20d20bfb992d178a2d3b307e0d8798c3
csenazmul <csenazmul77@gmail.com>
2025-03-21 23:33:56 +0600

    setup serializers

--- contactapp/serializers.py
+++ contactapp/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+from .models import Contact
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        # fields = '__all__'
+        fields = ['name', 'email', 'phone_number', 'address']

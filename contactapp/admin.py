47afc30f01b05c88c525ba221003be4c009f706b
csenazmul <csenazmul77@gmail.com>
2025-03-22 00:10:43 +0600

    contact model admin register

--- contactapp/admin.py
+++ contactapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Contact
 # Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone_number', 'address']
+    search_fields = ['name', 'email', 'phone_number']
+    list_filter = ['name', 'email', 'phone_number', 'address']
+    list_per_page = 10
+# Compare this snippet from contactapp/views.py:
+# from django.shortcuts import render   
+# from rest_framework import viewsets
+# from .models import Contact   
+# from .serializers import ContactSerializer
+#       
